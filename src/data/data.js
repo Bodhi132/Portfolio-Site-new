5cb582223b12de6b2bc2bced392a840922b1df7a
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2023-07-12 15:24:08 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+import shop from '../assets/projects/Shop-Mop.png'
+import sketch from '../assets/projects/sketch.png'
+import board from '../assets/projects/Dashboard.png'
+import clone from '../assets/projects/net-clone.png'
+import gym from '../assets/projects/Workout-app.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Shop-Mop(Desktop Application)",
+        image:shop,
+        github:"https://github.com/Bodhi132/Shop-Mop.git",
+        live:"https://64944cfcc341b22ed50dfa7d--thunderous-melba-080795.netlify.app/",
+    },
+    {
+        id:2,
+        name:"Open Board 2.0(Desktop App)",
+        image:sketch,
+        github:"https://github.com/Bodhi132/Open-Board-Clone.git",
+        live:"https://open-board-xfqc.onrender.com/",
+    },
+    {
+        id:3,
+        name:"Responsive Dashboard Using Next Js and Google Auth",
+        image:board,
+        github:"https://github.com/Bodhi132/listed-assignment.git",
+        live:"https://listed-assignment-sage.vercel.app/",
+    },
+    {
+        id:4,
+        name:"Netflix clone",
+        image:clone,
+        github:"https://github.com/Bodhi132/Netflix-Clone.git",
+        live:"https://videostreaming-six.vercel.app/",
+    },
+    {
+        id:5,
+        name:"Workout App",
+        image:gym,
+        github:"https://github.com/Bodhi132/Netflix-Clone.git",
+        live:"https://github.com/Bodhi132/workout-app.git",
+    },
+
+
+]
