0931c07ff5ecb61f0e0443d081eeed93e37944f3
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2023-07-17 08:59:58 +0530

    drive link change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1dLhcJtczB6Ane-t9XcCzKRHN4_M8-_zV/view?usp=drive_link'
+              href='https://drive.google.com/file/d/1yQ8JXOJmTKVcXNQp6xNCy8f66WkQKKAb/view?usp=sharing'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
