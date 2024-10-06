5a851ad5aa2bfdff5782ff3385e4a2349c2a6158
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2024-10-06 19:54:39 +0530

    new update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
           I'm a Web Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
+          I’m a frontend developer specializing in building (and occasionally
+          designing) functional and decent digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.
         </p>
         <div>
