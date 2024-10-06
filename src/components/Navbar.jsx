8f0e1ea14f88e83bbbf97a27c100d59203cfe55b
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2024-10-06 19:30:25 +0530

    new update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1ui_DrhdzyNWef1LptN7j5T5_2llGWlLa/view?usp=sharing'
+              href='https://drive.google.com/file/d/1frYG0QbqMmOsuOSrDC9jDDeQzVV-B_R1/view?usp=sharing'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
